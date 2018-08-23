3e11e0aa5bc429787a0d32c881b521ac9b349a85
AntonioCS <antoniocs@gmail.com>
2018-08-23 13:14:57 +0100

    Code for animations

--- Animation.cpp
+++ Animation.cpp
@@ -1,13 +1,90 @@
 #include "Animation.h"
+#include <iostream>
+#include <algorithm> // std::for_each
 
 namespace AcsGameEngine {
+    void Animation::applyToFrames(std::chrono::milliseconds value)
+    {
+        std::for_each(
+            m_frames.begin(),
+            m_frames.end(),
+            [&value](std::pair<Sprite, std::chrono::milliseconds> &frame) {
+                frame.second += value;
+            }
+        );
+    }
 
-	Animation::Animation()
-	{
-	}
+    Animation::Animation(
+        const std::string name,
+        const Texture & texture,
+        std::chrono::milliseconds duration) : m_name(name), m_texture(texture), m_duration(duration)
+    {
+    }
 
+    Animation::Animation(
+        const std::string name,
+        const Texture &texture,
+        SDL_Rect start,
+        unsigned totalFrames,
+        SDL_Rect moveBy,
+        std::chrono::milliseconds duration
+    ) : Animation(name, texture, duration)
+    {
+        while (totalFrames--) {
+            m_frames.emplace_back(Sprite{ m_texture, start }, duration);
 
-	Animation::~Animation()
-	{
-	}
+            start.x += moveBy.x;
+            start.y += moveBy.y;
+            start.w += moveBy.w;
+            start.h += moveBy.h;
+        }
+    }
+
+    size_t Animation::totalFrames() const noexcept
+    {
+        return m_frames.size();
+    }
+    void Animation::add(SDL_Rect rect, std::chrono::milliseconds duration)
+    {
+        m_frames.emplace_back(Sprite{ m_texture, rect }, duration);
+    }
+    void Animation::play()
+    {
+        m_timer.start();
+
+        m_position = 0;
+        m_running = true;
+    }
+
+    void Animation::stop() {
+        m_timer.stop();
+
+        m_running = false;
+        m_position = 0;
+    }
+
+    Sprite &Animation::get() {
+        if (m_running == false) {
+            play();
+        }
+
+        auto currentFrameDuration = m_frames.at(m_position).second;
+
+        if (m_timer.elapsed() > currentFrameDuration) {
+            m_timer.reset();
+            m_position = (m_position + 1) % m_frames.size();
+        }
+
+        return m_frames.at(m_position).first;
+    }
+    void Animation::delayBy(std::chrono::milliseconds value)
+    {
+        applyToFrames(value);
+    }
+
+    void Animation::speedUpBy(std::chrono::milliseconds value)
+    {
+        value *= -1;
+        applyToFrames(value);
+    }
 }
--- Animation.h
+++ Animation.h
@@ -1,12 +1,58 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+
+#include <vector>
+#include <string>
+#include <utility> // std::pair
+
+#include "Texture.h"
+#include "Sprite.h"
+#include "Util/Timer.h"
+
 
 namespace AcsGameEngine {
-	class Animation
-	{
-	public:
-		Animation();
-		~Animation();
-	};
+    using namespace Util;
+    class Animation
+    {
+        std::vector<std::pair<Sprite, std::chrono::milliseconds>> m_frames;
+        const std::string m_name;
+        const Texture &m_texture;
 
+        std::chrono::milliseconds m_duration;
+        unsigned int m_position{0};
+        bool m_running{ false };
+
+        Timer m_timer;
+
+        void applyToFrames(std::chrono::milliseconds);
+	public:
+        Animation(
+            const std::string name,
+            const Texture &texture,
+            std::chrono::milliseconds duration
+        );
+
+        Animation(
+            const std::string name,
+            const Texture &texture,
+            SDL_Rect start,
+            unsigned totalFrames,
+            SDL_Rect moveBy,
+            std::chrono::milliseconds duration
+        );
+
+        ~Animation() = default;
+
+        size_t totalFrames() const noexcept;
+
+        void add(SDL_Rect, std::chrono::milliseconds);
+
+        void play();
+        void stop();
+        Sprite &get();
+
+        void delayBy(std::chrono::milliseconds);
+        void speedUpBy(std::chrono::milliseconds);
+	};
 }
