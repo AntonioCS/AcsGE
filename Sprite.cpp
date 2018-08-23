996a0d6e21b74f9549cd3684cf0c0546a926e4da
AntonioCS <antoniocs@gmail.com>
2018-08-23 13:36:21 +0100

    Added SetCenter (not used do to issues with angle > 0), added angle, added flip

--- Sprite.cpp
+++ Sprite.cpp
@@ -4,101 +4,121 @@
 #include "Window.h"
 
 namespace AcsGameEngine {
-/*	Sprite::Sprite() : m_texture(nullptr)
-	{
 
-	}*/
+    Sprite::Sprite(const Texture &texture, SDL_Rect source) : m_texture(texture), m_source(source)
+    {
+        m_destination.w = m_source.w;
+        m_destination.h = m_source.h;
 
-	Sprite::Sprite(Texture &texture, SDL_Rect source) : m_texture(texture), m_source(source)
-	{
-		m_destination.w = m_source.w;
-		m_destination.h = m_source.h;
-	}	
+        m_center.x = m_destination.w / 2;
+        m_center.y = m_destination.h / 2;
+    }
 
-	Sprite::Sprite(Sprite&& other)
-		: m_texture(other.m_texture)
-		, m_destination(std::move(other.m_destination))
-		, m_source(std::move(other.m_source))
-	{
-	}
+    Sprite::Sprite(Sprite&& other)
+        : m_texture(other.m_texture)
+        , m_destination(std::move(other.m_destination))
+        , m_source(std::move(other.m_source))
+    {
+    }
 
-	Sprite::~Sprite()
-	{
-	}
+    void Sprite::setSourceDestination(SDL_Rect source, SDL_Rect destination)
+    {
+        m_source = std::move(source);
+        m_destination = std::move(destination);
+    }
 
-	void Sprite::setSourceDestination(SDL_Rect source, SDL_Rect destination)
-	{
-		m_source = std::move(source);
-		m_destination = std::move(destination);
-	}
+    void Sprite::setSource(const SDL_Rect& r)
+    {
+        setSourceXYWH(r.x, r.y, r.w, r.h);
+    }
 
-	void Sprite::setSource(const SDL_Rect& r)
-	{
-		setSourceXYWH(r.x, r.y, r.w, r.h);
-	}
+    void Sprite::setSourceXYWH(int x, int y, int w, int h)
+    {
+        m_source.x = x;
+        m_source.y = y;
+        m_source.w = w;
+        m_source.h = h;
+    }
 
-	void Sprite::setSourceXYWH(int x, int y, int w, int h)
-	{
-		m_source.x = x;
-		m_source.y = y;
-		m_source.w = w;
-		m_source.h = h;
-	}
+    void Sprite::setDestinationXYWH(int x, int y, int w, int h)
+    {
+        m_destination.x = x;
+        m_destination.y = y;
+        m_destination.w = w;
+        m_destination.h = h;
+    }
 
-	void Sprite::setDestinationXYWH(int x, int y, int w, int h)
-	{
-		m_destination.x = x;
-		m_destination.y = y;
-		m_destination.w = w;
-		m_destination.h = h;
-	}
+    void Sprite::setSourceXY(int x, int y)
+    {
+        m_source.x = x;
+        m_source.y = y;
+    }
 
-	void Sprite::setSourceXY(int x, int y)
-	{
-		m_source.x = x;
-		m_source.y = y;
-	}
+    void Sprite::setSourceWH(int w, int h)
+    {
+        m_source.w = w;
+        m_source.h = h;
+    }
 
-	void Sprite::setSourceWH(int w, int h)
-	{
-		m_source.w = w;
-		m_source.h = h;
-	}
+    void Sprite::setDestinationXY(int x, int y)
+    {
+        m_destination.x = x;
+        m_destination.y = y;
+    }
 
-	void Sprite::setDestinationXY(int x, int y)
-	{
-		m_destination.x = x;
-		m_destination.y = y;
-	}
+    void Sprite::setDestinationWH(int w, int h)
+    {
+        m_destination.w = w;
+        m_destination.h = h;
+    }
 
-	void Sprite::setDestinationWH(int w, int h)
-	{
-		m_destination.w = w;
-		m_destination.h = h;
-	}
+    void Sprite::setCenter()
+    {
+        m_destination.x = -1;
+        m_destination.y = -1;
+    }
 
-	void Sprite::setCenter()
-	{
-		m_destination.x = -1;
-		m_destination.y = -1;
-	}
-	//*/
+    void Sprite::angle(double angle) {
+        m_angle = angle;
+    }
 
-	std::pair<int, int> Sprite::getDestinationPoint() const noexcept
-	{
-		return { m_destination.x, m_destination.y };
-	}
-	SDL_Rect Sprite::getDestination() const noexcept
-	{
-		return m_destination;
-	}
-	SDL_Rect Sprite::getSource() const noexcept
-	{
-		return m_source;
-	}
-	const Texture & Sprite::getTexture() const noexcept
-	{
-		return m_texture;
-	}
+    double Sprite::angle() const noexcept {
+        return m_angle;
+    }
+
+    void Sprite::flip(SDL_RendererFlip state) {
+        m_flip = state;
+    }
+
+    SDL_RendererFlip Sprite::flip() const noexcept {
+        return m_flip;
+    }
+
+    void Sprite::centerPoint(SDL_Point point)
+    {
+        m_center = point;
+    }
+
+    SDL_Point Sprite::centerPoint() const noexcept
+    {
+        return m_center;
+    }
+
+    std::pair<int, int> Sprite::getDestinationPoint() const noexcept
+    {
+        return { m_destination.x, m_destination.y };
+    }
+    SDL_Rect Sprite::getDestination() const noexcept
+    {
+        return m_destination;
+    }
+    SDL_Rect Sprite::getSource() const noexcept
+    {
+        return m_source;
+    }
+    const Texture & Sprite::getTexture() const noexcept
+    {
+        return m_texture;
+    }
 
 } // namespace AcsGameEngine
--- Sprite.h
+++ Sprite.h
@@ -8,36 +8,48 @@
 namespace AcsGameEngine {
 
 
-	class Sprite {
-	private:
-		Texture & m_texture;// = nullptr;
-		SDL_Rect m_source{ 0, 0, 0, 0 };
-		SDL_Rect m_destination{ 0, 0, 0, 0 };
+    class Sprite {
+    private:
+        const Texture & m_texture;
+        SDL_Rect m_source{ 0, 0, 0, 0 };
+        SDL_Rect m_destination{ 0, 0, 0, 0 };
+        double m_angle{ 0 };
+        SDL_RendererFlip m_flip{ SDL_FLIP_NONE };
+        SDL_Point m_center;
 
-	public:
-		Sprite(Texture &texture, SDL_Rect);
-		Sprite(Sprite&& other);
-		virtual ~Sprite();
+    public:
+        Sprite(const Texture &texture, SDL_Rect);
+        Sprite(Sprite&& other);
+        ~Sprite() = default;
 
-		void setSourceDestination(SDL_Rect, SDL_Rect);
-		void setSource(const SDL_Rect&);
+        void setSourceDestination(SDL_Rect, SDL_Rect);
+        void setSource(const SDL_Rect&);
 
-		void setSourceXYWH(int, int, int, int);
-		void setDestinationXYWH(int, int, int, int);
+        void setSourceXYWH(int, int, int, int);
+        void setDestinationXYWH(int, int, int, int);
 
-		void setSourceXY(int, int);
-		void setDestinationXY(int, int);
+        void setSourceXY(int, int);
+        void setDestinationXY(int, int);
 
-		void setSourceWH(int, int);
-		void setDestinationWH(int, int);
+        void setSourceWH(int, int);
+        void setDestinationWH(int, int);
 
-		void setCenter();
+        void setCenter();
 
-		std::pair<int, int> getDestinationPoint() const noexcept;
-		SDL_Rect getDestination() const noexcept;
-		SDL_Rect getSource() const noexcept;
+        void angle(double);
+        double angle() const noexcept;
 
-		const Texture& getTexture() const noexcept;
-	};
+        void flip(SDL_RendererFlip);
+        SDL_RendererFlip flip() const noexcept;
+
+        void centerPoint(SDL_Point);
+        SDL_Point centerPoint() const noexcept;
+
+        std::pair<int, int> getDestinationPoint() const noexcept;
+        SDL_Rect getDestination() const noexcept;
+        SDL_Rect getSource() const noexcept;
+
+        const Texture& getTexture() const noexcept;
+    };
 }  // namespace AcsGameEngine
 #endif /* SPRITE_H */
