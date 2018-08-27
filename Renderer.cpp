e6161c61e67ff2e0c16ed8d45e386321546a6121
AntonioCS <antoniocs@gmail.com>
2018-08-27 22:43:12 +0100

    First implementation of circle

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,11 +1,18 @@
 #include <SDL2/SDL_image.h>
 #include <utility>
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <array>
+#include <utility> //std::pair
+#include <algorithm> // std::min, std::max
+#include <unordered_map>
 
 #include "Renderer.h"
 #include "Sprite.h"
 #include "Texture.h"
 #include "Window.h"
+
 
 
 namespace AcsGameEngine {
@@ -125,6 +132,104 @@ void Renderer::DrawPoint(int x, int y, const CList::Color &c)
 {
     DrawColor(c);
     SDL_RenderDrawPoint(getRawPointer(), x, y);
+}
+
+//https://gist.github.com/derofim/912cfc9161269336f722
+//https://en.wikipedia.org/wiki/Midpoint_circle_algorithm#C_Example
+void Renderer::DrawCircle(std::pair<int, int> center, int radius, const CList::Color & c, bool fill)
+{
+    auto[x0, y0] = center;
+    std::vector<std::pair<int, int>> points;
+    auto toRadians = [](int degree) -> double {
+        return degree * (M_PI / 180);
+    };
+
+    auto createPoints = [](int x0, int y0, int radius, std::vector<std::pair<int, int>> &points) -> void {
+        int x = radius - 1;
+        int y = 0;
+        int dx = 1;
+        int dy = 1;
+        int err = dx - (radius << 1);
+
+        while (x >= y)
+        {
+            points.emplace_back(x0 + x, y0 + y);
+            points.emplace_back(x0 + y, y0 + x);
+            points.emplace_back(x0 - y, y0 + x);
+            points.emplace_back(x0 - x, y0 + y);
+            points.emplace_back(x0 - x, y0 - y);
+            points.emplace_back(x0 - y, y0 - x);
+            points.emplace_back(x0 + y, y0 - x);
+            points.emplace_back(x0 + x, y0 - y);
+
+            if (err <= 0)
+            {
+                y++;
+                err += dy;
+                dy += 2;
+            }
+
+            if (err > 0)
+            {
+                x--;
+                dx += 2;
+                err += dx - (radius << 1);
+            }
+        }
+    };
+
+    //auto deg = 0;
+    /*
+    while (deg < 360) {
+        auto rad = toRadians(deg);
+        auto x2 = x + (radius * std::cos(rad));
+        auto y2 = y - (radius * std::sin(rad));
+
+        deg++;
+        */
+        //std::cout << "x2: " << x2 << " y2: " << y2 << " Degrees: " << deg << '\n';
+        /*
+        if (deg > 1) {
+            const auto &[last_x, last_y] = points.back();
+            auto a = std::min(last_y, static_cast<int>(y2));
+            auto b = std::max(last_y, static_cast<int>(y2));
+            auto res = b - a;
+
+            if (res > 1) {
+                while (res) {
+                    points.emplace_back(last_x, b - res--);
+                }
+            }
+        }*/
+
+      //  points.emplace_back(static_cast<int>(std::ceil(x2)), static_cast<int>(std::ceil(y2)));
+    //}
+
+    createPoints(x0, y0, radius, points);
+
+    if (fill) {
+        std::unordered_map<int, std::vector<int>> xPoints;
+
+        for (const auto&[x2, y2] : points) {
+            if (xPoints.count(y2)) {
+                xPoints[y2].push_back(x2);
+            }
+            else {
+                xPoints[y2] = std::vector<int>{ x2 };
+            }
+        }
+
+        for (const auto &[y, values] : xPoints) {
+            if (values.size() >= 2) {
+                const auto [xSmall, xBig] = std::minmax_element(values.begin(), values.end());
+                DrawLine(*xSmall, y, *xBig, y, CList::dark_blue);
+            }
+        }
+    }
+
+    for (const auto&[x2, y2] : points) {
+        DrawPoint(x2, y2, c);
+    }
 }
 
 Texture Renderer::make_texture(const std::string & path, const Util::ColorList::Color transparentColor)
