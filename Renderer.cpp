d88813c87939d1e16021df95984060697f24671d
AntonioCS <antoniocs@gmail.com>
2018-09-14 07:02:08 +0100

    Removed draw functions

--- Renderer.cpp
+++ Renderer.cpp
@@ -12,242 +12,144 @@
 #include "Sprite.h"
 #include "Texture.h"
 #include "Window.h"
-
+#include "Util/Color.h"
+#include "Util/ColorList.h"
 
 
 namespace AcsGameEngine {
 
+    using Util::Color;
+    using Util::ColorList;
 
-Renderer::Renderer(const Window& window, int index, Uint32 flags)
-    : m_renderer(createRendererPointer(window.getRawPointer(), index, flags), SDL_DestroyRenderer)
-    , m_window(window)
-{
 
-    if (m_renderer == nullptr) {
-        throw std::runtime_error(std::string{ "Unable to initialize renderer: " }
-            + std::string{ SDL_GetError() });
-    }
+    Renderer::Renderer(const Window& window, int index, Uint32 flags)
+        : m_renderer(createRendererPointer(window.getRawPointer(), index, flags), SDL_DestroyRenderer)
+        , m_window(window)
+    {
 
-	SDL_SetRenderDrawBlendMode(getRawPointer(), SDL_BLENDMODE_BLEND);
-}
-
-SDL_Renderer* Renderer::createRendererPointer(SDL_Window* w, int index, Uint32 flags)
-{
-    return SDL_CreateRenderer(w, index, flags);
-}
-
-Renderer & Renderer::operator=(const Renderer & other)
-{
-	if (&other != this) {
-
-	}
-	return *this;
-}
-
-Renderer & Renderer::operator=(Renderer && other)
-{
-	if (&other != this) {
-
-	}
-	return *this;
-}
-
-void Renderer::DrawColor(const CList::Color& color)
-{
-    if (color != CList::_nocolor) {
-        m_currentColor = color;
-        DrawColor(color.r, color.g, color.b, color.a);
-    }
-}
-
-void Renderer::DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) const
-{
-    SDL_SetRenderDrawColor(getRawPointer(), r, g, b, alpha);
-}
-
-void Renderer::Clear(const Util::ColorList::Color& c)
-{
-    DrawColor(c);
-    SDL_RenderClear(getRawPointer());
-}
-
-void Renderer::Present() const
-{
-    SDL_RenderPresent(getRawPointer());
-}
-
-void Renderer::DrawRect(const SDL_Rect rect, const CList::Color& c, bool fill)
-{
-    DrawColor(c);
-    fill ? SDL_RenderFillRect(getRawPointer(), &rect) : SDL_RenderDrawRect(getRawPointer(), &rect);
-}
-
-void Renderer::DrawRect(const Sprite &sprite, const CList::Color & c, bool fill)
-{
-    DrawRect(sprite.getDestination(), c, fill);
-}
-
-SDL_Renderer* Renderer::getRawPointer() const
-{
-    return m_renderer.get();
-}
-
-const Window& Renderer::getWindow() const
-{
-    return m_window;
-}
-
-bool Renderer::hasColor() const noexcept
-{
-    return m_currentColor != CList::_nocolor;
-}
-
-Renderer::CList::Color Renderer::getColor() const noexcept
-{
-    return m_currentColor;
-}
-
-void AcsGameEngine::Renderer::DrawSprite(const Sprite& sprite) const noexcept
-{
-    const Texture &texture = sprite.getTexture();
-    //SDL_RenderCopy(getRawPointer(), texture.getRawPointer(), &(sprite.getSource()), &(sprite.getDestination()));
-    SDL_RenderCopyEx(
-        getRawPointer(),
-        texture.getRawPointer(),
-        &(sprite.getSource()),
-        &(sprite.getDestination()),
-        sprite.angle(),
-        nullptr,//&(sprite.centerPoint()), @TODO - Fix this. If the angle > 0 then this messes up the whole thing unless it's null
-        sprite.flip()
-    );
-}
-
-void Renderer::DrawLine(int x1, int y1, int x2, int y2, const CList::Color & c)
-{
-    DrawColor(c);
-    SDL_RenderDrawLine(getRawPointer(), x1, y1, x2, y2);
-}
-
-void Renderer::DrawPoint(int x, int y, const CList::Color &c)
-{
-    DrawColor(c);
-    SDL_RenderDrawPoint(getRawPointer(), x, y);
-}
-
-//https://gist.github.com/derofim/912cfc9161269336f722
-//https://en.wikipedia.org/wiki/Midpoint_circle_algorithm#C_Example
-void Renderer::DrawCircle(std::pair<int, int> center, int radius, const CList::Color & c, bool fill)
-{
-    auto[x0, y0] = center;
-    std::vector<std::pair<int, int>> points;
-    auto toRadians = [](int degree) -> double {
-        return degree * (M_PI / 180);
-    };
-
-    auto createPoints = [](int x0, int y0, int radius, std::vector<std::pair<int, int>> &points) -> void {
-        int x = radius - 1;
-        int y = 0;
-        int dx = 1;
-        int dy = 1;
-        int err = dx - (radius << 1);
-
-        while (x >= y)
-        {
-            points.emplace_back(x0 + x, y0 + y);
-            points.emplace_back(x0 + y, y0 + x);
-            points.emplace_back(x0 - y, y0 + x);
-            points.emplace_back(x0 - x, y0 + y);
-            points.emplace_back(x0 - x, y0 - y);
-            points.emplace_back(x0 - y, y0 - x);
-            points.emplace_back(x0 + y, y0 - x);
-            points.emplace_back(x0 + x, y0 - y);
-
-            if (err <= 0)
-            {
-                y++;
-                err += dy;
-                dy += 2;
-            }
-
-            if (err > 0)
-            {
-                x--;
-                dx += 2;
-                err += dx - (radius << 1);
-            }
-        }
-    };
-
-    //auto deg = 0;
-    /*
-    while (deg < 360) {
-        auto rad = toRadians(deg);
-        auto x2 = x + (radius * std::cos(rad));
-        auto y2 = y - (radius * std::sin(rad));
-
-        deg++;
-        */
-        //std::cout << "x2: " << x2 << " y2: " << y2 << " Degrees: " << deg << '\n';
-        /*
-        if (deg > 1) {
-            const auto &[last_x, last_y] = points.back();
-            auto a = std::min(last_y, static_cast<int>(y2));
-            auto b = std::max(last_y, static_cast<int>(y2));
-            auto res = b - a;
-
-            if (res > 1) {
-                while (res) {
-                    points.emplace_back(last_x, b - res--);
-                }
-            }
-        }*/
-
-      //  points.emplace_back(static_cast<int>(std::ceil(x2)), static_cast<int>(std::ceil(y2)));
-    //}
-
-    createPoints(x0, y0, radius, points);
-
-    if (fill) {
-        std::unordered_map<int, std::vector<int>> xPoints;
-
-        for (const auto&[x2, y2] : points) {
-            if (xPoints.count(y2)) {
-                xPoints[y2].push_back(x2);
-            }
-            else {
-                xPoints[y2] = std::vector<int>{ x2 };
-            }
+        if (m_renderer == nullptr) {
+            throw std::runtime_error(std::string{ "Unable to initialize renderer: " }
+            +std::string{ SDL_GetError() });
         }
 
-        for (const auto &[y, values] : xPoints) {
-            if (values.size() >= 2) {
-                const auto [xSmall, xBig] = std::minmax_element(values.begin(), values.end());
-                DrawLine(*xSmall, y, *xBig, y, CList::dark_blue);
-            }
+        SDL_SetRenderDrawBlendMode(getRawPointer(), SDL_BLENDMODE_BLEND);
+    }
+
+    SDL_Renderer* Renderer::createRendererPointer(SDL_Window* w, int index, Uint32 flags) const noexcept
+    {
+        return SDL_CreateRenderer(w, index, flags);
+    }
+
+    Renderer & Renderer::operator=(const Renderer & other)
+    {
+        if (&other != this) {
+
+        }
+        return *this;
+    }
+
+    Renderer & Renderer::operator=(Renderer && other) noexcept
+    {
+        if (&other != this) {
+
+        }
+        return *this;
+    }
+
+    void Renderer::DrawColor(const Color& color) const noexcept
+    {
+        if (color != ColorList::_nocolor) {
+            //m_currentColor = color;
+            DrawColor(color.r, color.g, color.b, color.a);
         }
     }
 
-    for (const auto&[x2, y2] : points) {
-        DrawPoint(x2, y2, c);
-    }
-}
-
-Texture Renderer::make_texture(const std::string & path, const Util::ColorList::Color transparentColor)
-{
-    SDL_Surface *tmp = IMG_Load(path.c_str());
-
-    if (tmp == nullptr) {
-        throw std::string{ "Unable to load image: " } +path;
+    void Renderer::DrawColor(const Color *color) const noexcept
+    {
+        DrawColor(*color);
     }
 
-    if (transparentColor != Util::ColorList::_nocolor) {
-        SDL_SetColorKey(tmp, SDL_TRUE, SDL_MapRGB(tmp->format, transparentColor.r, transparentColor.g, transparentColor.b));
+    void Renderer::DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) const noexcept
+    {
+        SDL_SetRenderDrawColor(getRawPointer(), r, g, b, alpha);
     }
 
-	SDL_Texture *t = SDL_CreateTextureFromSurface(getRawPointer(), tmp);
-	SDL_FreeSurface(tmp);
+    void Renderer::Clear(const Color &c) const noexcept
+    {
+        DrawColor(c);
+        Clear();
+    }
+    void Renderer::Clear() const noexcept
+    {
+        SDL_RenderClear(getRawPointer());
+    }
 
-	return Texture{ t };
-}
+    void Renderer::Present() const noexcept
+    {
+        SDL_RenderPresent(getRawPointer());
+    }
+
+    void Renderer::DrawRect(const SDL_Rect rect, bool fill) const noexcept
+    {
+        fill ? SDL_RenderFillRect(getRawPointer(), &rect) : SDL_RenderDrawRect(getRawPointer(), &rect);
+    }
+
+    void Renderer::DrawRect(const Sprite &sprite, bool fill) const noexcept
+    {
+        DrawRect(sprite.getDestination(), fill);
+    }
+
+    SDL_Renderer* Renderer::getRawPointer() const noexcept
+    {
+        return m_renderer.get();
+    }
+
+    const Window& Renderer::getWindow() const noexcept
+    {
+        return m_window;
+    }
+
+    void AcsGameEngine::Renderer::DrawSprite(const Sprite& sprite) const noexcept
+    {
+        const Texture &texture = sprite.getTexture();
+        //SDL_RenderCopy(getRawPointer(), texture.getRawPointer(), &(sprite.getSource()), &(sprite.getDestination()));
+        SDL_RenderCopyEx(
+            getRawPointer(),
+            texture.getRawPointer(),
+            &(sprite.getSource()),
+            &(sprite.getDestination()),
+            sprite.angle(),
+            nullptr,//&(sprite.centerPoint()), @TODO - Fix this. If the angle > 0 then this messes up the whole thing unless it's null
+            sprite.flip()
+        );
+    }
+
+    void Renderer::DrawLine(int x1, int y1, int x2, int y2) const noexcept
+    {
+        SDL_RenderDrawLine(getRawPointer(), x1, y1, x2, y2);
+    }
+
+    void Renderer::DrawPoint(int x, int y) const noexcept
+    {
+        SDL_RenderDrawPoint(getRawPointer(), x, y);
+    }
+
+    Texture Renderer::make_texture(const std::string & path, const Color &transparentColor) const
+    {
+        SDL_Surface *tmp = IMG_Load(path.c_str());
+
+        if (tmp == nullptr) {
+            throw std::string{ "Unable to load image: " } +path;
+        }
+
+        if (transparentColor != ColorList::_nocolor) {
+            SDL_SetColorKey(tmp, SDL_TRUE, SDL_MapRGB(tmp->format, transparentColor.r, transparentColor.g, transparentColor.b));
+        }
+
+        SDL_Texture *t = SDL_CreateTextureFromSurface(getRawPointer(), tmp);
+        SDL_FreeSurface(tmp);
+
+        return Texture{ t };
+    }
 
 } // namespace AcsGameEngine
--- Renderer.h
+++ Renderer.h
@@ -1,53 +1,51 @@
 #pragma once
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <memory>
 #include <string>
-#include <unordered_map>
-
-#include "Util/ColorList.h"
 
 namespace AcsGameEngine {
+
     class Window;
     class Texture;
     class Sprite;
 
+    namespace Util {
+        class Color;
+    }
+
+    using Util::Color;
+
     class Renderer {
-        using CList = Util::ColorList;
+        std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> m_renderer;
+        const Window& m_window;
+        SDL_Renderer* createRendererPointer(SDL_Window* w, int index, Uint32 flags) const noexcept;
 
     public:
         explicit Renderer(const Window& window, int index = -1, Uint32 flags = SDL_RENDERER_ACCELERATED);
         Renderer(const Renderer& orig) = delete;
         Renderer(Renderer &&) = default;
         Renderer& operator=(const Renderer& other);
-        Renderer& operator=(Renderer && other);
+        Renderer& operator=(Renderer && other) noexcept;
 
-        void DrawColor(const Util::ColorList::Color&);
-        void DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha = 255) const;
-        void Clear(const CList::Color& c = CList::_nocolor);
-        void Present() const;
-        void DrawRect(SDL_Rect, const CList::Color &c = CList::_nocolor, bool fill = false);
-        void DrawRect(const Sprite &Sprite, const CList::Color &c = CList::_nocolor, bool fill = false);
+        void DrawColor(const Color&) const noexcept;
+        void DrawColor(const Color*) const noexcept;
+        void DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha = 255) const noexcept;
+
+        void Clear(const Color &) const noexcept;
+        void Clear() const noexcept;
+        void Present() const noexcept;
+
+        void DrawRect(SDL_Rect, bool fill = false) const noexcept;
+        void DrawRect(const Sprite &, bool fill = false) const noexcept;
+        void DrawLine(int x1, int y1, int x2, int y2) const noexcept;
+        void DrawPoint(int x, int y) const noexcept;
+
         void DrawSprite(const Sprite &) const noexcept;
-        void DrawLine(int x1, int y1, int x2, int y2, const CList::Color &c = CList::_nocolor);
-        void DrawPoint(int x, int y, const CList::Color &c = CList::_nocolor);
-        void DrawCircle(std::pair<int, int> center, int radius, const CList::Color &c = CList::_nocolor, bool fill = false);
 
-        Texture make_texture(const std::string & path, const Util::ColorList::Color transparentColor = CList::_nocolor);
+        Texture make_texture(const std::string & path, const Color &transparentColor) const;
 
-        SDL_Renderer* getRawPointer() const;
-        const Window& getWindow() const;
-
-        bool hasColor() const noexcept;
-
-        CList::Color getColor() const noexcept;
-
-    private:
-        std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> m_renderer;
-        const Window& m_window;
-        SDL_Renderer* createRendererPointer(SDL_Window* w, int index, Uint32 flags);
-
-        CList::Color m_currentColor = CList::_nocolor;
+        SDL_Renderer* getRawPointer() const noexcept;
+        const Window& getWindow() const noexcept;
     };
 } //namespace AcsGameEngine
