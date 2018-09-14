91d240559afec59952f999c488e5bb9b0fac5e14
AntonioCS <antoniocs@gmail.com>
2018-09-14 07:46:12 +0100

    Formatting and some new overloads

--- Util/Vector2D.h
+++ Util/Vector2D.h
@@ -1,37 +1,41 @@
 #pragma once
 
 #include <utility>
+#include <chrono>
 
 namespace AcsGameEngine::Util {
+	struct Vector2D {
 
-	class Vector2D {
+		float x{};
+		float y{};
 
-		float m_x{};
-		float m_y{};
-	public:
+        Vector2D();
 		Vector2D(float x, float y);
-		Vector2D(const Vector2D& other);
-		Vector2D(Vector2D &&other);
-
+		Vector2D(const Vector2D &other);
+		Vector2D(Vector2D &&other) noexcept;
+        ~Vector2D() = default;
 		Vector2D &operator=(const Vector2D &other);
-		Vector2D& operator=(Vector2D &&other);
-		~Vector2D();
+		Vector2D &operator=(Vector2D &&other) noexcept;
 
-		//++Vector2D
-		Vector2D& operator++();
-
-		//Vector2D++
-		Vector2D operator++(int);
-
+		Vector2D& operator++(); //++Vector2D
+		Vector2D operator++(int); //Vector2D++
 		Vector2D &operator+=(const Vector2D& rhs);
-
-		friend Vector2D operator+(Vector2D lhs, const Vector2D& rhs);
-	
 		Vector2D &operator-=(const Vector2D& rhs);
+		Vector2D operator*(float s) const noexcept;
+		Vector2D operator/(float s) const noexcept;
+		Vector2D operator-() const noexcept;
+        Vector2D operator+(const Vector2D& v) const;
+        Vector2D operator-(const Vector2D& v) const;
+        //Vector2D operator*(std::chrono::milliseconds ms) const noexcept;
 
-		friend Vector2D operator-(Vector2D const &lhs, const Vector2D& rhs);
+        Vector2D normalized() const noexcept;
+
+        float length() const noexcept;
+        float lengthSqr() const noexcept;
 
 		float getX() const noexcept;
 		float getY() const noexcept;
+        std::pair<float, float> getXY() const noexcept;
+        std::pair<int, int> getXYint() const noexcept;
 	};
 }
