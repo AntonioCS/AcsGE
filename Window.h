b202840fc4200606a4b1aca7774eff9082c6173f
AntonioCS <antoniocs@gmail.com>
2018-10-19 08:00:19 +0100

    Removed commeted out code. Clean up. Added struct that has default params

--- Window.h
+++ Window.h
@@ -10,47 +10,51 @@ namespace AcsGameEngine {
     //https://wiki.libsdl.org/SDL_CreateWindow
 
     class Window {
+        struct WindowDefaults
+        {
+            inline static const std::string title{ "SDL2 Window" };
+            inline static const int x{ SDL_WINDOWPOS_CENTERED };
+            inline static const int y{ SDL_WINDOWPOS_CENTERED };
+            inline static const int w{ 800 };
+            inline static const int h{ 600 };
+            inline static const uint32_t flags{ SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN };
+        };
     public:
-        explicit Window(std::string title = "SDL2 Window", int x = SDL_WINDOWPOS_CENTERED, int y = SDL_WINDOWPOS_CENTERED, int w = 800, int h = 600, uint32_t flags = SDL_WINDOW_OPENGL);
+        static constexpr int WINDOW_CENTERED = SDL_WINDOWPOS_CENTERED;
 
+        explicit Window(
+            const std::string &title = WindowDefaults::title,
+            int x = WindowDefaults::x,
+            int y = WindowDefaults::y,
+            int w = WindowDefaults::w,
+            int h = WindowDefaults::h,
+            uint32_t flags = WindowDefaults::flags
+        );
+
+        ~Window() = default;
         Window(const Window &orig) = delete;
+        Window(Window &&orig) = delete;
 
         int getX() const noexcept;
-
         int getY() const noexcept;
 
         std::pair<int, int> getPosition() const noexcept;
-
         std::pair<int, int> getSize() const noexcept;
 
         int getWidth() const noexcept;
-
         int getHeight() const noexcept;
 
         inline std::string getTitle() const noexcept;
 
-        void setTitle(const std::string& title);
+        void setTitle(const std::string& title) const noexcept;
+
+        void showWindow() const noexcept;
+        void hideWindow() const noexcept;
 
         SDL_Window *getRawPointer() const;
-    private:        
-        std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> m_window;        
-
-
-/*
-        void applyNewPosition() const noexcept {
-            if (m_window != nullptr) {
-                SDL_SetWindowPosition(m_window, m_x, m_y);
-            }
-        }
-
-        void applyNewSize() const noexcept {
-            if (m_window != nullptr) {
-                SDL_SetWindowSize(m_window, m_w, m_h);
-            }
-        }
-*/
+    private:
+        std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> m_window;
         SDL_Window *createWindowPointer(const std::string& title, int x, int y, int w, int h, uint32_t flags);
     };
 }  // namespace AcsGameEngine
 #endif // WINDOW_H
-
