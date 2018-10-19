b7595013e0335a81c50f29c20598d23c47784f2c
AntonioCS <antoniocs@gmail.com>
2018-10-19 07:49:32 +0100

    Fixed name

--- ECS/Systems/ColisionSystem.cpp
+++ ECS/Systems/ColisionSystem.cpp
@@ -1,12 +1,12 @@
 #include "ColisionSystem.h"
 
-#include "../Components/ColisionComponent.h"
+#include "../Components/CollisionComponent.h"
 #include "../Components/PositionComponent.h"
 
 namespace AcsGameEngine::ECS {
 
 	void ColisionSystem::init() {
-		m_vecRef = m_entityManager.findByComponent<ColisionComponent, PositionComponent>();
+		m_vecRef = m_entityManager.findByComponent<CollisionComponent, PositionComponent>();
 	}
 
 	void ColisionSystem::update(float dt) {
