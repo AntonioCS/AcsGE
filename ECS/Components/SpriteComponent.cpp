f69235a13428c6cee92dc75bd4761a7ead396bc6
AntonioCS <antoniocs@gmail.com>
2018-11-21 07:25:59 +0000

    Using static_cast

--- ECS/Components/SpriteComponent.cpp
+++ ECS/Components/SpriteComponent.cpp
@@ -1,14 +1,15 @@
 #include "SpriteComponent.h"
-#include <utility>
 #include "../../Texture.h"
+#include <SDL2/SDL.h>
 
 namespace AcsGameEngine::ECS {
-	SpriteComponent::SpriteComponent(Texture &texture, SDL_Rect s, int x, int y) : m_sprite(texture, s)
-	{
-		getSprite().setDestinationXY(x, y);
-	}
-	Sprite & SpriteComponent::getSprite()
-	{
-		return m_sprite;
-	}
+    SpriteComponent::SpriteComponent(Texture &texture, AcsGameEngine::Util::Shapes::Rectangle rect, int x, int y)
+        : m_sprite(texture, SDL_Rect{ static_cast<int>(rect.origin.x), static_cast<int>(rect.origin.y), static_cast<int>(rect.dimensions.width), static_cast<int>(rect.dimensions.height) })
+    {
+        getSprite().setDestinationXY(x, y);
+    }
+    Sprite & SpriteComponent::getSprite()
+    {
+        return m_sprite;
+    }
 }
