ddef5dfcddd9ace0a95b8aa58d40dd5dc6271429
AntonioCS <antoniocs@gmail.com>
2018-11-22 08:07:49 +0000

    Fixed issue with vector of systems. Should have been a unique_ptr

--- ECS/SystemManager.cpp
+++ ECS/SystemManager.cpp
@@ -2,19 +2,15 @@
 #include <algorithm>
 
 namespace AcsGameEngine::ECS {
-    void SystemManager::add(System&& system)
-    {
-        m_systems.push_back(system);
-    }
 
     void SystemManager::init()
     {
         std::for_each(
             std::begin(m_systems), 
             std::end(m_systems),
-            [](const System &system)
+            [](std::unique_ptr<System> &system)
             {
-                
+                system->init();
             }
         );
     }
--- ECS/SystemManager.h
+++ ECS/SystemManager.h
@@ -1,17 +1,23 @@
 ﻿#pragma once
 
 #include <vector>
+#include <memory>
 #include "System.h"
 
 namespace AcsGameEngine::ECS {
     class SystemManager
     {
-        std::vector <System> m_systems;
     public:
+        template<typename T, typename... TArgs>
+        T& addSystem(TArgs&&... mArgs) {
+            m_systems.push_back(std::make_unique<T>(std::forward<TArgs>(mArgs)...));
+            return static_cast<T&>(*m_systems.back());
+        }
 
-        void add(System &&system);
         void init();
         void update(float dt);
         void render();
+    private:
+        std::vector <std::unique_ptr<System>> m_systems;
     };
 }
