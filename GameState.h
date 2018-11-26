ddf9ec6df6c709e55387f5dfba4fb1c37ca15bfa
AntonioCS <antoniocs@gmail.com>
2018-11-26 07:41:24 +0000

    Setters and getters

--- GameState.h
+++ GameState.h
@@ -10,29 +10,54 @@ namespace AcsGameEngine
 {
     class GameStateManager;
     class Renderer;
+    class Window;
     class EventManager;
 
     class GameState
     {
     public:
-        GameState(GameStateManager& gsm) : m_gameStateManager(gsm)
-        {
-        }
-
+        GameState() = default;
         virtual ~GameState() = default;
 
         virtual void init();
         virtual void handleEvents();
         virtual void update(std::chrono::milliseconds deltaTime);
-        virtual void render(Renderer &renderer);
+        virtual void render();
+
+        void setGameStateManager(GameStateManager *gsm)
+        {
+            m_gameStateManager = gsm;
+        }
+
+        void setRenderer(Renderer *renderer)
+        {
+            m_renderer = renderer;
+        }
+
+        void setWindow(Window *window)
+        {
+            m_window = window;
+        }
+
+        GameStateManager *getGameStateManager() const
+        {
+            return m_gameStateManager;
+        }
+
+        Renderer *getRenderer() const
+        {
+            return m_renderer;
+        }
+
+        Window *getWindow() const
+        {
+            return m_window;
+        }
 
     protected:
         using SysPtr = std::unique_ptr<ECS::System>;
         using SysFunc = std::function<void(SysPtr &sys)>;
-    private:
-        GameStateManager& m_gameStateManager;
-        std::vector<SysPtr> m_systems;
-    protected:
+
         GameStateManager &getGSM() const noexcept;
 
         void loopAction(SysFunc func)
@@ -43,5 +68,10 @@ namespace AcsGameEngine
                 func
             );
         }
+    private:
+        GameStateManager *m_gameStateManager;
+        Renderer *m_renderer;
+        Window *m_window;
+        std::vector<SysPtr> m_systems;
     };
 }
