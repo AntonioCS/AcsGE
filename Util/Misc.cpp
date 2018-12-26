81e944e14fa6ebb39fec86ef49f6810d302b708e
AntonioCS <antoniocs@gmail.com>
2018-12-26 21:11:22 +0000

    Util

---
+++ Util/Misc.cpp
@@ -0,0 +1,26 @@
+﻿#include "Misc.h"
+#include <algorithm> //std::for_each
+#include "../Texture.h"
+#include "../Renderer.h"
+#include "../Sprite.h"
+#include "../AssetManager.h"
+
+namespace AcsGameEngine::Util {
+    void Misc::imageToSprites(        
+        AssetManager &assetManager,
+        const std::string& imagePath,
+        const std::vector<std::pair<std::string, SDL_Rect>>& coords,
+        std::unordered_map<std::string, std::unique_ptr<Sprite>>& destination)
+    {
+        auto texture = assetManager.makeTexture(imagePath);                
+
+        std::for_each(
+            coords.begin(),
+            coords.end(),
+            [&texture, &destination](const std::pair<std::string, SDL_Rect> &pair)
+            {
+                destination[pair.first] = std::make_unique<AcsGameEngine::Sprite>(*texture, pair.second);
+            }
+        );
+    }
+}
---
+++ Util/Misc.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+
+#include <vector>
+#include <string>
+#include <utility> //std::pair
+#include <unordered_map>
+#include <memory> //unique_ptr
+
+#include <SDL2/SDL.h>
+
+namespace AcsGameEngine
+{
+    class Renderer;
+    class Sprite;
+    class AssetManager;
+
+}
+
+
+namespace AcsGameEngine::Util {
+
+    class Misc
+    {
+    public:
+        static void imageToSprites(
+            AssetManager &assetManager,
+            const std::string &imagePath,
+            const std::vector<std::pair<std::string, SDL_Rect>> &coords,
+            std::unordered_map<std::string, std::unique_ptr<Sprite>> &destination);
+    };
+}
