e9733ae88d2f567e635c90192ea3bd53a66332de
AntonioCS <antoniocs@gmail.com>
2018-12-26 21:10:38 +0000

    Uses renderer

---
+++ Uses/UsesRenderer.cpp
@@ -0,0 +1,14 @@
+﻿#include "UsesRenderer.h"
+
+namespace AcsGameEngine::Uses {
+
+    void UsesRenderer::setRenderer(Renderer* r)
+    {
+        m_renderer = r;
+    }
+
+    Renderer* UsesRenderer::getRenderer() const
+    {
+        return m_renderer;
+    }
+}
---
+++ Uses/UsesRenderer.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+
+namespace AcsGameEngine {
+    class Renderer;
+}
+
+namespace AcsGameEngine::Uses {
+
+    class UsesRenderer
+    {
+    public:
+        void setRenderer(Renderer *r);
+        Renderer *getRenderer() const;
+    private:
+        Renderer *m_renderer = nullptr;
+    };
+}
