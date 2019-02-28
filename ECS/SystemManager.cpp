d493911eb0ef2f1ea89d21e56dbadadd87b53177
AntonioCS <antoniocs@gmail.com>
2019-02-28 08:57:56 +0000

    Making sure all systems are setup correctly

--- ECS/SystemManager.cpp
+++ ECS/SystemManager.cpp
@@ -1,16 +1,51 @@
 ﻿#include "SystemManager.h"
 #include <algorithm>
+#include <functional>
 
+#define LOOP_SYSTEMS(anonFunc) std::for_each( \
+                                std::begin(m_systems), \
+                                std::end(m_systems), \
+                                anonFunc \
+                            )
 namespace AcsGameEngine::ECS {
-
-    void SystemManager::init()
+    void loopSystems(std::vector<std::unique_ptr<System>> &vec,
+        std::function<void(std::unique_ptr<System> &system)> func)
     {
         std::for_each(
-            std::begin(m_systems), 
-            std::end(m_systems),
-            [](std::unique_ptr<System> &system)
-            {
+            std::begin(vec),
+            std::end(vec),
+            func
+        );
+    }
+}
+namespace AcsGameEngine::ECS {
+    void SystemManager::init()
+    {
+        loopSystems(
+            m_systems,
+            [](std::unique_ptr<System> &system) {
                 system->init();
+            }
+        );
+    }
+
+    void SystemManager::update(std::chrono::milliseconds ts)
+    {
+
+        loopSystems(
+            m_systems,
+            [&ts](std::unique_ptr<System> &system) {
+                system->update(ts);
+            }
+        );
+    }
+
+    void SystemManager::render()
+    {
+        loopSystems(
+            m_systems,
+            [](std::unique_ptr<System> &system) {
+                system->render();
             }
         );
     }
--- ECS/SystemManager.h
+++ ECS/SystemManager.h
@@ -2,22 +2,48 @@
 
 #include <vector>
 #include <memory>
+#include <type_traits>
+#include <cassert>
+#include <chrono>
 #include "System.h"
 
-namespace AcsGameEngine::ECS {
+namespace AcsGameEngine
+{
+    class Renderer;
+    class Window;
+}
+
+namespace AcsGameEngine::ECS
+{
+    class EntityManager;
+
     class SystemManager
     {
     public:
-        template<typename T, typename... TArgs>
-        T& addSystem(TArgs&&... mArgs) {
-            m_systems.push_back(std::make_unique<T>(std::forward<TArgs>(mArgs)...));
-            return static_cast<T&>(*m_systems.back());
+        SystemManager(Renderer *r, Window *w, EntityManager *em) : m_renderer(r), m_window(w), m_entityManager(em) {}
+
+        template <typename T, typename... TArgs>
+        T& addSystem(TArgs&&... mArgs)
+        {
+            static_assert(std::is_base_of<System, T>::value, "T must inherit from System");
+            auto &res = m_systems.emplace_back(std::make_unique<T>(std::forward<TArgs>(mArgs)...));
+
+            res.get()->setEntityManager(m_entityManager);
+            res.get()->setRenderer(m_renderer);
+            res.get()->setWindow(m_window);
+
+            return static_cast<T&>(*res);
         }
 
         void init();
-        void update(float dt);
+        void update(std::chrono::milliseconds);
         void render();
+
     private:
-        std::vector <std::unique_ptr<System>> m_systems;
+        Renderer *m_renderer{nullptr};
+        Window *m_window{nullptr};
+        EntityManager *m_entityManager{nullptr};
+
+        std::vector<std::unique_ptr<System>> m_systems;
     };
 }
