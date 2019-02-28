021db6addecd52b3628ce3b96d857c6c9bf38d34
AntonioCS <antoniocs@gmail.com>
2019-02-28 08:33:38 +0000

    Keyboard Manager

---
+++ KeyboardManager.cpp
@@ -0,0 +1,45 @@
+﻿#include "KeyboardManager.h"
+
+namespace AcsGameEngine {
+
+    KeyboardManager& AcsGameEngine::KeyboardManager::getInstance()
+    {
+        static KeyboardManager km;
+        return km;
+    }
+
+    void KeyboardManager::onKeyDown(SDL_Event& e)
+    {
+        const auto key = e.key.keysym.sym;
+        setKeyState(key, true);
+    }
+
+    void KeyboardManager::onKeyUp(SDL_Event& e)
+    {
+        const auto key = e.key.keysym.sym;
+        setKeyState(key, false);
+    }
+
+    bool KeyboardManager::isPressed(Keys k) const
+    {
+        const auto k2 = m_keys.find(k);
+
+        if (k2 != m_keys.end()) {
+            return k2->second;
+        }
+
+        return false;
+    }
+
+    bool KeyboardManager::hasKey(SDL_Keycode k) const noexcept
+    {
+        return m_keysMappings.find(k) != m_keysMappings.end();
+    }
+
+    void KeyboardManager::setKeyState(SDL_Keycode sdlKey, bool state) noexcept
+    {
+        if (hasKey(sdlKey)) {
+            m_keys[m_keysMappings[sdlKey]] = state;
+        }
+    }
+}
---
+++ KeyboardManager.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include <SDL2/SDL.h>
+#include <unordered_map>
+
+namespace AcsGameEngine {
+
+    class KeyboardManager
+    {
+    public:
+        enum class Keys
+        {
+            ARROW_KEY_UP,
+            ARROW_KEY_DOWN,
+            ARROW_KEY_LEFT,
+            ARROW_KEY_RIGHT
+        };
+
+        KeyboardManager(KeyboardManager const &) = delete;
+        KeyboardManager(KeyboardManager &&) = delete;        
+
+        void operator=(KeyboardManager const &) = delete;
+        void operator=(KeyboardManager &&) = delete;
+
+        ~KeyboardManager() = default;
+
+
+        static KeyboardManager& getInstance();
+
+        void onKeyDown(SDL_Event& e);
+
+        void onKeyUp(SDL_Event& e);
+
+        bool isPressed(Keys k) const;
+
+    private:
+        KeyboardManager() = default;
+
+        std::unordered_map<Keys, bool> m_keys;
+        std::unordered_map<SDL_Keycode, Keys> m_keysMappings{
+            {SDLK_UP, Keys::ARROW_KEY_UP},
+            {SDLK_DOWN, Keys::ARROW_KEY_DOWN},
+            {SDLK_LEFT, Keys::ARROW_KEY_LEFT},
+            {SDLK_RIGHT, Keys::ARROW_KEY_RIGHT}
+        };
+
+        bool hasKey(SDL_Keycode k) const noexcept;
+
+        void setKeyState(SDL_Keycode sdlKey, bool state) noexcept;
+    };
+}
