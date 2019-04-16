8d84715b10f0b074b19f3513c92b52be087deffd
AntonioCS <antoniocs@gmail.com>
2019-04-16 07:29:35 +0100

    Methods renaming

--- Game.h
+++ Game.h
@@ -6,38 +6,50 @@
 #include "AssetManager.h"
 #include "EventManager.h"
 #include "ECS/EntityManager.h"
+#include "Util/Vector2D.h"
+#include "Util/Size.h"
+
 
 namespace AcsGameEngine {
     class Renderer;
     class Window;
     class GameStateManager;
+    class GameState;
     class AssetManager;
 
     class Game
     {
     public:
-        Game(std::string windowTitle, std::pair<int, int> windowXY, std::pair<int, int> windowWH);
+        //Game(std::string windowTitle);
+        Game(std::string windowTitle, Util::Vector2D windowXY, Util::Size windowWH);
         ~Game() = default;
 
-        void Init();
-        void Cleanup();
-        void Run();
-        bool Running() const noexcept;
-        void Quit();
+        void init();
+        void cleanup();
+        void run();
+        bool running() const noexcept;
+        void quit();
+
+        Util::Color getClearColor() const noexcept;
+        void setClearColor(Util::Color);
 
         Renderer *getRenderer() const;
         Window *getWindow() const;
-        GameStateManager *getGSM();
+        GameStateManager *getGameStateManager() const;
         EventManager *getEventManager();
         ECS::EntityManager *getEntityManager();
         AssetManager *getAssetManager();
 
+        GameState *getCurrentGameState() const;
+
     private:
-        struct WindowData
-        {
+        void registerGameEvents();
+
+    private:
+        struct WindowData {
             std::string windowTitle;
-            std::pair<int, int> windowXY;
-            std::pair<int, int> windowWH;
+            Util::Vector2D windowXY;
+            Util::Size windowWH;
         } m_windowData;
 
         std::unique_ptr<Window> m_window;
@@ -48,5 +60,9 @@ namespace AcsGameEngine {
         AssetManager m_assetManager;
 
         bool m_running{ true };
+        bool m_windowCentered{ false };
+
+        Util::Color m_clearColor{ Util::ColorList::white };
+
     };
 }
