32062b0633663b300eb77d8d697da1672a42da04
AntonioCS <antoniocs@gmail.com>
2019-04-23 07:30:14 +0100

    Added a few functions

--- Game.cpp
+++ Game.cpp
@@ -14,6 +14,7 @@
 #include "GameStateManager.h"
 #include "Util/Timer.h"
 #include "Util/ColorList.h"
+#include "KeyboardManager.h"
 
 template<typename... Types>
 void throwException(Types... args) {
@@ -24,14 +25,14 @@ void throwException(Types... args) {
 }
 
 namespace AcsGameEngine {
-    Game::Game(std::string windowTitle, std::pair<int, int> windowXY, std::pair<int, int> windowWH)
+    Game::Game(std::string windowTitle, Util::Vector2D windowXY, Util::Size windowWH)
     {
         m_windowData.windowTitle = windowTitle;
         m_windowData.windowXY = windowXY;
         m_windowData.windowWH = windowWH;
     }
 
-    void Game::Init()
+    void Game::init()
     {
         if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
             throwException("Unable to initialize SDL: ", SDL_GetError());
@@ -46,85 +47,96 @@ namespace AcsGameEngine {
             throwException("SDL_ttf could not initialize! SDL_ttf Error: ", TTF_GetError());
         }
 
-        auto[x, y] = m_windowData.windowXY;
-        auto[w, h] = m_windowData.windowWH;
+        auto[x, y] = m_windowData.windowXY.getXYint();
+        auto[w, h] = m_windowData.windowWH.getWHint();
 
-        m_window = std::make_unique<Window>(m_windowData.windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h);
+        m_window = std::make_unique<Window>(m_windowData.windowTitle, x, y, w, h);
         m_renderer = std::make_unique<Renderer>(*m_window);
         m_gameStateManager = std::make_unique<GameStateManager>(*this);
         m_assetManager.setRenderer(&*m_renderer);
     }
 
-    void Game::Cleanup()
+    void Game::cleanup()
     {
         TTF_Quit();
         IMG_Quit();
         SDL_Quit();
     }
 
-    void Game::Run()
+    void Game::run()
     {
         // https://gist.github.com/mariobadr/673bbd5545242fcf9482
         using namespace std::chrono_literals;
         using clock = std::chrono::high_resolution_clock;
         using std::chrono::milliseconds;
 
-        Util::Timer timer;
-        timer.start();
-
-        constexpr milliseconds timeStep{ 16ms };
-        auto accumulator{ 0ms };
+        constexpr auto timeStep{ 16ms };
+        constexpr Util::Timer::ms maxAccumulator{ 250ms };
+        Util::Timer::ms accumulator{ 0ms };
         auto frameTime{ 0ms }; //deltaTime I prefer the word frameTime
+        Util::Timer timer;
 
-        if (m_window->isHidden())
-        {
+        if (m_window->isHidden()) {
             m_window->showWindow();
         }
 
-        m_eventManager.onQuit(
-            [this](SDL_Event &event) {
-                m_running = false;
-            }
-        );
+        registerGameEvents();
+
+        timer.start();
 
         while (m_running)
         {
-            frameTime = timer.elapsed();
+            auto currentGameState = getCurrentGameState();
+
+            accumulator += timer.elapsed();
             timer.reset();
-            accumulator += frameTime;
 
             m_eventManager.processEvents();
 
-            if (m_gameStateManager->getCurrentState() == nullptr)
-            {
-                throw std::runtime_error{ "Current state is a null" };
+            while (accumulator >= timeStep) {
+                currentGameState->preUpdate(timeStep);
+                currentGameState->update(timeStep);
+                currentGameState->postUpdate(timeStep);
+
+                accumulator -= timeStep;
+
+                if (accumulator >= maxAccumulator) {
+                    accumulator = 0ms;
+                }
             }
 
-            auto &currentState = *(m_gameStateManager->getCurrentState());
+            m_renderer->clear(m_clearColor);
 
-            do
-            {
-                currentState.update(timeStep);
-                accumulator -= timeStep;
-            } while (accumulator >= timeStep);
-
-            m_renderer->clear(Util::ColorList::white);
-            //m_renderer->Clear();
-
-            currentState.render();
+            currentGameState->preRender();
+            currentGameState->render();
+            currentGameState->postRender();
 
             m_renderer->present();
+
+            if (currentGameState->hasChangeToNewState()) {
+                m_gameStateManager->changeState(currentGameState->getNewStateToChange());
+            }
         }
     }
 
-    bool Game::Running() const noexcept
+    bool Game::running() const noexcept
     {
         return m_running;
     }
 
-    void Game::Quit()
+    void Game::quit()
     {
         m_running = false;
+    }
+
+    Util::Color Game::getClearColor() const noexcept
+    {
+        return m_clearColor;
+    }
+
+    void Game::setClearColor(Util::Color c)
+    {
+        m_clearColor = c;
     }
 
     Renderer *Game::getRenderer() const
@@ -137,7 +149,7 @@ namespace AcsGameEngine {
         return m_window.get();
     }
 
-    GameStateManager *Game::getGSM()
+    GameStateManager *Game::getGameStateManager() const
     {
         return m_gameStateManager.get();
     }
@@ -155,5 +167,61 @@ namespace AcsGameEngine {
     AssetManager* Game::getAssetManager()
     {
         return &m_assetManager;
+    }
+
+    GameState* Game::getCurrentGameState() const
+    {
+        const auto currentState = m_gameStateManager->getCurrentState();
+
+        if (currentState == nullptr) {
+            throw std::runtime_error{ "Current game state is a null" };
+        }
+
+        return currentState;
+    }
+
+    void Game::registerGameEvents()
+    {
+        constexpr auto critical = Util::MediatorMultiPriority::critical;
+
+        m_eventManager.onQuit([this](SDL_Event &event) {
+            quit();
+            return true;
+        }, critical);
+
+
+        auto &km = KeyboardManager::getInstance();
+
+        m_eventManager.onKeyDown([&km](SDL_Event& e)
+        {
+            km.onKeyDown(e);
+            return true;
+        }, critical);
+
+        m_eventManager.onKeyUp([&km](SDL_Event& e)
+        {
+            km.onKeyUp(e);
+            return true;
+        }, critical);
+
+        m_eventManager.onEvent(SDL_MOUSEBUTTONDOWN, [](SDL_Event &event)
+        {
+            return true;
+        }, critical);
+
+        m_eventManager.onEvent(SDL_MOUSEBUTTONUP, [](SDL_Event &event)
+        {
+            return true;
+        }, critical);
+
+        m_eventManager.onEvent(SDL_MOUSEMOTION, [](SDL_Event &event)
+        {
+            return true;
+        }, critical);
+
+        m_eventManager.onEvent(SDL_MOUSEWHEEL, [](SDL_Event &event)
+        {
+            return true;
+        }, critical);
     }
 }
