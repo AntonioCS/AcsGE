fc7fdeddcfd05b05aed8b45dace32864f5e12889
AntonioCS <antoniocs@gmail.com>
2019-04-23 07:26:49 +0100

    Added color key

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -2,8 +2,10 @@
 #include "Renderer.h"
 #include <stdexcept> //runtime_error
 #include <algorithm> //std::for_each
+#include "Sprite2.h"
 
-namespace AcsGameEngine {
+namespace AcsGameEngine
+{
     AssetManager::AssetManager(Renderer* r)
     {
         this->setRenderer(r);
@@ -14,15 +16,15 @@ namespace AcsGameEngine {
         return m_textures.find(name) != m_textures.end();
     }
 
-    Texture* AssetManager::makeTexture(const std::string& texturePath)
+    Texture* AssetManager::makeTexture(const std::string& texturePath, Util::Color colorKey)
     {
-        return makeTexture(texturePath, texturePath);
+        return makeTexture(texturePath, texturePath, colorKey);
     }
 
-    AcsGameEngine::Texture* AssetManager::makeTexture(const std::string &name, const std::string& texturePath)
+    AcsGameEngine::Texture* AssetManager::makeTexture(const std::string& name, const std::string& texturePath, Util::Color colorKey)
     {
         if (!hasTexture(name)) {
-            m_textures.insert({ name,  getRenderer()->makeTexture(texturePath) });
+            m_textures.insert({name, getRenderer()->makeTexture(texturePath, colorKey)});
             return &m_textures[name];
         }
 
@@ -43,38 +45,89 @@ namespace AcsGameEngine {
         return m_sprites.find(name) != m_sprites.end();
     }
 
-    void AssetManager::makeSprites(
-        const std::string& textureName,
-        const std::vector<std::pair<std::string, SDL_Rect>>& nameAndCoords
-    )
+    void AssetManager::makeSprites(const std::string& textureName, const std::vector<MultiData>& nameAndCoords)
     {
+
         auto texture = getTexture(textureName);
 
         std::for_each(
             nameAndCoords.begin(),
             nameAndCoords.end(),
-            [&texture, this](const std::pair<std::string, SDL_Rect> &pair)
-            {
-                auto[name, coords] = pair;
+            [&texture, this](const MultiData& data)
+        {
+            auto[name, vec, size] = data;
 
-                if (!this->hasSprite(name)) {
-                    this->m_sprites.insert({ pair.first, Sprite(texture, coords) });
-                }
-                else {
-                    throw std::runtime_error{
-                        std::string{ "Sprite with name: " } + name + std::string{" already exists"}
-                    };
-                }
+            if (!this->hasSprite(name)) {
+                this->m_sprites.insert({ name, Sprite2(texture, vec, size) });
             }
+            else {
+                throw std::runtime_error{ "Sprite with name: " + name + " already exists" };
+            }
+        }
         );
+
     }
 
-    Sprite* AssetManager::getSprite(const std::string& name)
+    Sprite2* AssetManager::getSprite(const std::string& name)
     {
         if (hasSprite(name)) {
             return &m_sprites[name];
         }
 
         return nullptr;
+    }
+
+    Font *AssetManager::loadFont(const std::string &name, const std::string& path, int size)
+    {
+        const auto font = TTF_OpenFont(path.c_str(), size);
+
+        if (font == nullptr) {
+            throw std::runtime_error{"Unable to load font from path: "  + path};
+        }
+
+        m_fonts[name].emplace(size, Font(font));
+        return &(m_fonts[name][size]);    
+    }
+
+    Font *AssetManager::loadFont(const std::string& path, int size)
+    {
+        return loadFont(path, path, size);
+    }
+
+    Font* AssetManager::getFont(const std::string& name, int size)
+    {
+        /*auto res = m_fonts.find(name);
+        
+        if (res != m_fonts.end) {
+            auto res2 = res->second.find(size);
+
+            if (res2 != res->second.end()) {
+                return &(res2->second);
+            }
+        }*/
+
+        return nullptr;
+    }
+
+    void AssetManager::loadFontMap(
+        const std::string& name, const std::string& pathToFontSprite,
+        const std::string& pathToFontMap, Util::Color colorKey)
+    {
+        makeTexture(name, pathToFontSprite, colorKey);
+        m_fontAtlas.emplace(name, FontAtlas{pathToFontMap});
+    }
+
+    bool AssetManager::hasFontMap(const std::string& name)
+    {
+        return m_fontAtlas.find(name) != m_fontAtlas.end();
+    }
+
+    std::optional<std::pair<Texture*, FontAtlas>> AssetManager::getFontMap(const std::string& name)
+    {
+        if (hasTexture(name) && hasFontMap(name)) {
+            return std::make_pair(getTexture(name), m_fontAtlas[name]);
+        }
+
+        return {};
     }
 }
--- AssetManager.h
+++ AssetManager.h
@@ -2,31 +2,62 @@
 
 #include <unordered_map>
 #include <string>
+#include <optional>
 
 #include "Uses/UsesRenderer.h"
 #include "Texture.h"
 #include "Sprite.h"
+#include "Sprite2.h"
+#include "Font.h"
+#include "Util/Size.h"
+#include "Util/Vector2D.h"
+#include "Util/Color.h"
+#include "Util/ColorList.h"
+#include "FontAtlas.h"
 
-namespace AcsGameEngine  {
-    class Renderer;    
+
+namespace AcsGameEngine {
+
+    class Renderer;
 
     class AssetManager : public Uses::UsesRenderer
     {
     public:
+        using Size = AcsGameEngine::Util::Size;
+        using Vector2D = AcsGameEngine::Util::Vector2D;
+        using MultiData = std::tuple<std::string, Vector2D, Size>;        
+    public:
         AssetManager() = default;
         AssetManager(Renderer *r);
 
-        bool hasTexture(const std::string &name) const;
-        Texture *makeTexture(const std::string &texturePath);
-        Texture *makeTexture(const std::string &name, const std::string &texturePath);
-        Texture *getTexture(const std::string &texturePath);
+        ~AssetManager() = default;
 
-        bool hasSprite(const std::string &name) const;
-        void makeSprites(const std::string &textureName, const std::vector<std::pair<std::string, SDL_Rect>>& nameAndCoords);
-        Sprite *getSprite(const std::string &name);
+        bool hasTexture(const std::string &name) const;
+        Texture *makeTexture(const std::string &texturePath, Util::Color colorKey = Util::ColorList::_nocolor);
+        Texture *makeTexture(const std::string &name, const std::string &texturePath, Util::Color colorKey = Util::ColorList::_nocolor);
+        Texture *getTexture(const std::string &name);
+
+        bool hasSprite(const std::string &name) const;        
+        void makeSprites(const std::string &textureName, const std::vector<MultiData>& nameAndCoords);
+        Sprite2 *getSprite(const std::string &name);
+
+        Font *loadFont(const std::string &name, const std::string &path, int size);
+        Font *loadFont(const std::string &path, int size);
+        Font *getFont(const std::string &name, int size);
+        Font *getFont(const std::string &name);
+
+        void loadFontMap(const std::string &name, const std::string &pathToFontSprite, const std::string &pathToFontMap, Util::Color colorKey = Util::ColorList::_nocolor);
+        bool hasFontMap(const std::string &name);
+        std::optional<std::pair<Texture*, FontAtlas>> getFontMap(const std::string &name);
 
     private:
         std::unordered_map<std::string, Texture> m_textures;
-        std::unordered_map<std::string, Sprite> m_sprites;
+        std::unordered_map<std::string, Sprite2> m_sprites;
+
+        //<font name + size, Font>>
+        //std::unordered_map<std::string, Font> m_fonts;
+        std::unordered_map<std::string, std::unordered_map<int,Font>> m_fonts;
+
+        std::unordered_map<std::string, FontAtlas> m_fontAtlas;
     };
 }
